bf78184515164e2e1b00f77633efb740b7d7d62a
Kevin McGinn <kmcginn14@gmail.com>
2017-01-03 13:22:56 -0800

    initial day 7 sketching

---
+++ 2016/day07/ip.py
@@ -0,0 +1,35 @@
+'''
+--- Day 7: Internet Protocol Version 7 ---
+
+While snooping around the local network of EBHQ, you compile a list of IP addresses (they're IPv7,
+of course; IPv6 is much too limited). You'd like to figure out which IPs support TLS
+(transport-layer snooping).
+
+An IP supports TLS if it has an Autonomous Bridge Bypass Annotation, or ABBA. An ABBA is any
+four-character sequence which consists of a pair of two different characters followed by the
+reverse of that pair, such as xyyx or abba. However, the IP also must not have an ABBA within any
+hypernet sequences, which are contained by square brackets.
+
+For example:
+
+abba[mnop]qrst supports TLS (abba outside square brackets).
+abcd[bddb]xyyx does not support TLS (bddb is within square brackets, even though xyyx is outside
+square brackets).
+aaaa[qwer]tyui does not support TLS (aaaa is invalid; the interior characters must be different).
+ioxxoj[asdfgh]zxcvbn supports TLS (oxxo is outside square brackets, even though it's within a
+larger string).
+How many IPs in your puzzle input support TLS?
+'''
+
+def is_abba(abba_str):
+    """Returns true if 4 character string consists of a pair of two different characters followed
+    by the reverse of that pair"""
+    if len(abba_str) != 4:
+        raise Exception
+
+def main():
+    """Execution of solution"""
+    pass
+
+if __name__ == "__main__":
+    main()
