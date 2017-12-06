3b11e678098050b7748ab44cba427b94cb8c0251
Kevin McGinn <kmcginn14@gmail.com>
2017-12-06 00:12:34 -0800

    WIP for 2017 day 3 part 1

---
+++ 2017/day03/spiral_memory.py
@@ -0,0 +1,41 @@
+"""
+from: http://adventofcode.com/2017/day/3
+
+--- Day 3: Spiral Memory ---
+You come across an experimental new kind of memory stored on an infinite two-dimensional grid.
+Each square on the grid is allocated in a spiral pattern starting at a location marked 1 and then
+counting up while spiraling outward. For example, the first few squares are allocated like this:
+17  16  15  14  13
+18   5   4   3  12
+19   6   1   2  11
+20   7   8   9  10
+21  22  23---> ...
+While this is very space-efficient (no squares are skipped), requested data must be carried back to
+square 1 (the location of the only access port for this memory system) by programs that can only
+move up, down, left, or right. They always take the shortest path: the Manhattan Distance between
+the location of the data and square 1.
+For example:
+Data from square 1 is carried 0 steps, since it's at the access port.
+Data from square 12 is carried 3 steps, such as: down, left, left.
+Data from square 23 is carried only 2 steps: up twice.
+Data from square 1024 must be carried 31 steps.
+How many steps are required to carry the data from the square identified in your puzzle input all
+the way to the access port?
+
+MY NOTES:
+the "sides" of the square made by the spiral increase by 2 each time - so all ODD numbers
+then the bottom right of a full square is the AREA of that square, or the max of that "ring"
+
+"""
+
+def ring_number(x):
+    pass    
+
+def main():
+    """Solve the problem!"""
+    with open('input.txt') as input_file:
+        data = input_file.read()
+    ring = ring_number(data)
+
+if __name__ == "__main__":
+    main()
