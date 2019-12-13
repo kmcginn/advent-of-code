dc56aa28c9d5aba988f0bf021d49b5aa459f54e7
Kevin McGinn <kmcginn14@gmail.com>
2019-12-13 01:00:47 -0800

    complete solution for 2019 day 9 part 1
    - add relative mode for operation parameters
    - add relative base offset operation
    - support accessing memory outside program

--- 2019/day09/intcode_sensor.py
+++ 2019/day09/intcode_sensor.py
@@ -61,6 +61,7 @@ keycode does it produce?
 """
 
 import os
+from collections import defaultdict
 
 def list_digits(number):
     """Takes up to a 6 digit int and returns a list of each digit in order"""
@@ -73,7 +74,7 @@ def list_digits(number):
     digits.append(number)
     return digits
 
-def get_param_value(mode, memory, ptr):
+def get_param_value(mode, memory, ptr, relative_base):
     """Returns the value of the parameter at memory[ptr] based on the mode"""
     # position mode
     if mode == 0:
@@ -83,11 +84,11 @@ def get_param_value(mode, memory, ptr):
         return memory[ptr]
     # relative mode
     elif mode == 2:
-        raise NotImplementedError
+        return memory[memory[ptr] + relative_base]
     else:
         raise Exception
 
-def get_write_param_value(mode, memory, ptr):
+def get_write_param_value(mode, memory, ptr, relative_base):
     """Returns the value of the paramter at memory[ptr] based on the mode, for a writing paramter"""
     # position mode
     if mode == 0:
@@ -97,13 +98,14 @@ def get_write_param_value(mode, memory, ptr):
         # immediate mode is not supported
         raise Exception
     elif mode == 2:
-        raise NotImplementedError
+        return memory[ptr] + relative_base
     else:
         raise Exception
 
 def run_intcode(memory, input_list, instr_ptr=0, input_ptr=0, previous_output=None):
     """Run an Intcode program from memory"""
     output = previous_output
+    relative_base = 0
     while instr_ptr < len(memory):
         instruction = memory[instr_ptr]
         digits = list_digits(instruction)
@@ -118,18 +120,18 @@ def run_intcode(memory, input_list, instr_ptr=0, input_ptr=0, previous_output=No
 
         # HALT
         if opcode == 99:
-            return (output, -1, -1)
+            return
 
         # ADD - num1, num2, store
         if opcode == 1:
             # process num1
-            num1 = get_param_value(param_modes[0], memory, instr_ptr + 1)
+            num1 = get_param_value(param_modes[0], memory, instr_ptr + 1, relative_base)
 
             # process num2
-            num2 = get_param_value(param_modes[1], memory, instr_ptr + 2)
+            num2 = get_param_value(param_modes[1], memory, instr_ptr + 2, relative_base)
 
             # process store
-            store = get_write_param_value(param_modes[2], memory, instr_ptr + 3)
+            store = get_write_param_value(param_modes[2], memory, instr_ptr + 3, relative_base)
 
             memory[store] = num1 + num2
             instr_ptr = instr_ptr + 4
@@ -137,20 +139,20 @@ def run_intcode(memory, input_list, instr_ptr=0, input_ptr=0, previous_output=No
         # MULTIPLY - num1, num2, store
         elif opcode == 2:
             # process num1
-            num1 = get_param_value(param_modes[0], memory, instr_ptr + 1)
+            num1 = get_param_value(param_modes[0], memory, instr_ptr + 1, relative_base)
 
             # process num2
-            num2 = get_param_value(param_modes[1], memory, instr_ptr + 2)
+            num2 = get_param_value(param_modes[1], memory, instr_ptr + 2, relative_base)
 
             # process store
-            store = get_write_param_value(param_modes[2], memory, instr_ptr + 3)
+            store = get_write_param_value(param_modes[2], memory, instr_ptr + 3, relative_base)
 
             memory[store] = num1 * num2
             instr_ptr = instr_ptr + 4
 
         # INPUT - position
         elif opcode == 3:
-            position = get_write_param_value(param_modes[0], memory, instr_ptr + 1)
+            position = get_write_param_value(param_modes[0], memory, instr_ptr + 1, relative_base)
 
             memory[position] = input_list[input_ptr]
             input_ptr = input_ptr + 1
@@ -158,18 +160,18 @@ def run_intcode(memory, input_list, instr_ptr=0, input_ptr=0, previous_output=No
 
         # OUTPUT - position
         elif opcode == 4:
-            output = get_param_value(param_modes[0], memory, instr_ptr + 1)
+            output = get_param_value(param_modes[0], memory, instr_ptr + 1, relative_base)
 
             instr_ptr = instr_ptr + 2
-            return (output, instr_ptr, input_ptr)
+            print(output)
 
         # JUMP-IF-TRUE - test, new_ptr
         elif opcode == 5:
             # process test
-            test = get_param_value(param_modes[0], memory, instr_ptr + 1)
+            test = get_param_value(param_modes[0], memory, instr_ptr + 1, relative_base)
 
             # process new_ptr
-            new_ptr = get_param_value(param_modes[1], memory, instr_ptr + 2)
+            new_ptr = get_param_value(param_modes[1], memory, instr_ptr + 2, relative_base)
 
             if test != 0:
                 instr_ptr = new_ptr
@@ -179,10 +181,10 @@ def run_intcode(memory, input_list, instr_ptr=0, input_ptr=0, previous_output=No
         # JUMP-IF-FALSE - test, new_ptr
         elif opcode == 6:
             # process test
-            test = get_param_value(param_modes[0], memory, instr_ptr + 1)
+            test = get_param_value(param_modes[0], memory, instr_ptr + 1, relative_base)
 
             # process new_ptr
-            new_ptr = get_param_value(param_modes[1], memory, instr_ptr + 2)
+            new_ptr = get_param_value(param_modes[1], memory, instr_ptr + 2, relative_base)
 
             if test == 0:
                 instr_ptr = new_ptr
@@ -192,13 +194,13 @@ def run_intcode(memory, input_list, instr_ptr=0, input_ptr=0, previous_output=No
         # LESS THAN - num1, num2, store
         elif opcode == 7:
             # process num1
-            num1 = get_param_value(param_modes[0], memory, instr_ptr + 1)
+            num1 = get_param_value(param_modes[0], memory, instr_ptr + 1, relative_base)
 
             # process num2
-            num2 = get_param_value(param_modes[1], memory, instr_ptr + 2)
+            num2 = get_param_value(param_modes[1], memory, instr_ptr + 2, relative_base)
 
             # process store
-            store = get_write_param_value(param_modes[2], memory, instr_ptr + 3)
+            store = get_write_param_value(param_modes[2], memory, instr_ptr + 3, relative_base)
 
             if num1 < num2:
                 memory[store] = 1
@@ -209,19 +211,26 @@ def run_intcode(memory, input_list, instr_ptr=0, input_ptr=0, previous_output=No
         # EQUALS - num1, num2, store
         elif opcode == 8:
             # process num1
-            num1 = get_param_value(param_modes[0], memory, instr_ptr + 1)
+            num1 = get_param_value(param_modes[0], memory, instr_ptr + 1, relative_base)
 
             # process num2
-            num2 = get_param_value(param_modes[1], memory, instr_ptr + 2)
+            num2 = get_param_value(param_modes[1], memory, instr_ptr + 2, relative_base)
 
             # process store
-            store = get_write_param_value(param_modes[2], memory, instr_ptr + 3)
+            store = get_write_param_value(param_modes[2], memory, instr_ptr + 3, relative_base)
 
             if num1 == num2:
                 memory[store] = 1
             else:
                 memory[store] = 0
             instr_ptr = instr_ptr + 4
+
+        # RELATIVE BASE OFFSET - offset
+        elif opcode == 9:
+            offset = get_param_value(param_modes[0], memory, instr_ptr + 1, relative_base)
+
+            relative_base += offset
+            instr_ptr = instr_ptr + 2
 
         # unknown opcode
         else:
@@ -233,7 +242,14 @@ def main():
     script_dir = os.path.dirname(__file__)
     file_path = os.path.join(script_dir, './input.txt')
     with open(file_path) as input_file:
-        pass
+        init_memory = list(map(int, input_file.read().split(',')))
+    # in order to support a memory larger than the program,
+    # copy program to a defaultdict with default value of 0
+    memory = defaultdict(lambda: 0)
+    for i in range(0, len(init_memory)):
+        memory[i] = init_memory[i]
+    input_list = [1]
+    run_intcode(memory, input_list)
 
 if __name__ == "__main__":
     main()
